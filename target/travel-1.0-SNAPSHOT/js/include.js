095108a8d936b5953a9b68dde84898c753bba27e
handing <handing0221@163.com>
2020-04-28 13:00:21 +0800

    删除target文件夹，修改.ignore文件

--- target/travel-1.0-SNAPSHOT/js/include.js
+++
@@ -1,8 +0,0 @@
-$(function () {
-    $.get("header.html",function (data) {
-        $("#header").html(data);
-    });
-    $.get("footer.html",function (data) {
-        $("#footer").html(data);
-    });
-});
